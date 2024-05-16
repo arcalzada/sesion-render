f83c7e530237fb8c4d4ef76d0af5e65ac70946d2
Álvaro Ruiz <alvarorurca.96@gmail.com>
2024-05-16 11:05:21 +0200

    Add whiteNoiseDependacy

--- myFirstApiRest/settings.py
+++ myFirstApiRest/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware', #para habilitar las llamadas desde react
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myFirstApiRest.urls'

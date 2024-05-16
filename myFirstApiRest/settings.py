d4d30b21b916b8555e2bae56f18922b77225cc64
Álvaro Ruiz <alvarorurca.96@gmail.com>
2024-05-16 10:50:04 +0200

    Add secret key y env

--- myFirstApiRest/settings.py
+++ myFirstApiRest/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^_cf5o%i%&$@-(!m2q^1wz*^83=!%98!w(czw@du&hkfayzne!'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-^_cf5o%i%&$@-(!m2q^1wz*^83=!%98!w(czw@du&hkfayzne!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []
@@ -154,7 +154,3 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
